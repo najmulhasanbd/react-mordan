1367e317ae6e6ee5a26f9980683133098345d000
najmul57 <najmulcse247@gmail.com>
2025-07-17 09:15:43 +0600

    dfd

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
   );
 };
 
